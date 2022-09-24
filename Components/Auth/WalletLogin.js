7cdb67bb12d8ade9168f5b7b9fde7fc76e5c630e
Sahad <govardhan.n@codingmart.com>
2022-09-24 19:26:55 +0530

    commit

--- Components/Auth/WalletLogin.js
+++ Components/Auth/WalletLogin.js
@@ -15,23 +15,21 @@ function WalletLogin() {
 
     const connector = useWalletConnect();
     const { width, height } = Dimensions.get("screen");
-    const theme = useSelector((state) => { return state.themeReducer.value.theme })
-    // console.log(cart)
+    const { theme, primary_Color, text_Color, btn_Color } = useSelector((state) => { return state.themeReducer.value })
     const dispatch = useDispatch();
 
 
-    // console.log(connector)
 
     if (!connector.connected) {
 
         return (
-            <SafeAreaView style={{ display: 'flex', flex: 1, backgroundColor: theme === "dark" ? "black" : "white" }}>
+            <SafeAreaView style={{ display: 'flex', flex: 1, backgroundColor: primary_Color }}>
                 <StatusBar hidden barStyle={theme === "dark" ? "light-content" : "dark-content"} />
 
                 <View >
-                    <Text style={{ fontFamily: "monster", color: theme !== "dark" ? "black" : "white", fontSize: height * 0.15, top: height * 0.01, letterSpacing: height * 0.005, position: "absolute" }}> NFT </Text>
-                    <Text style={{ fontFamily: "monster", color: theme !== "dark" ? "black" : "white", fontSize: height * 0.12, top: height * 0.2, position: "absolute" }}>  MARKET</Text>
-                    <Text style={{ fontFamily: "monster", color: theme !== "dark" ? "black" : "white", fontSize: height * 0.12, top: height * 0.2, position: "absolute" }}>  MARKET</Text>
+                    <Text style={{ fontFamily: "monster", color: text_Color, fontSize: height * 0.15, top: height * 0.01, letterSpacing: height * 0.005, position: "absolute" }}> NFT </Text>
+                    <Text style={{ fontFamily: "monster", color: text_Color, fontSize: height * 0.12, top: height * 0.2, position: "absolute" }}>  MARKET</Text>
+                    <Text style={{ fontFamily: "monster", color: text_Color, fontSize: height * 0.12, top: height * 0.2, position: "absolute" }}>  MARKET</Text>
 
 
                     <Image source={gem} style={{ height: 45, width: 45, top: height * 0.4, left: width * 0.2, position: "absolute" }} resizeMode={"center"} />
@@ -45,10 +43,10 @@ function WalletLogin() {
                     <View style={[StyleSheet.absoluteFillObject]}><Image source={require("../../assets/images/alien.png")} resizeMode={"center"} style={{ height: height, width: width }} /></View>
 
 
-                    <TouchableOpacity style={{ position: "absolute" }} onPress={() => { dispatch(themeController()) }}><Text style={{ color: theme === "dark" ? "white" : "black" }}>Dark</Text></TouchableOpacity>
+                    <TouchableOpacity style={{ position: "absolute" }} onPress={() => { dispatch(themeController()) }}><Text style={{ color: text_Color }}>Dark</Text></TouchableOpacity>
 
-                    <View style={{ top: height * 0.78, width: width * 0.8, alignSelf: "center", backgroundColor: theme === "dark" ? "black" : "white", justifyContent: "center", alignItems: "center", }}>
-                        <TouchableOpacity onPress={() => { connector.connect() }} style={{ backgroundColor: theme === "dark" ? "black" : "white", width: width * 0.9, height: height * 0.08, justifyContent: "center", alignItems: "center", elevation: 10 }}><Image source={theme === "dark" ? darkPic : lightPic} resizeMode={"contain"} style={{ height: height * 0.06, width: width * 0.8, }} /></TouchableOpacity>
+                    <View style={{ top: height * 0.78, width: width * 0.8, alignSelf: "center", backgroundColor: primary_Color, justifyContent: "center", alignItems: "center", }}>
+                        <TouchableOpacity onPress={() => { connector.connect() }} style={{ backgroundColor: btn_Color, width: width * 0.9, height: height * 0.08, justifyContent: "center", alignItems: "center", elevation: 10 }}><Image source={theme === "dark" ? darkPic : lightPic} resizeMode={"contain"} style={{ height: height * 0.06, width: width * 0.8, }} /></TouchableOpacity>
 
                     </View>
                 </View>
--- features/reduxStore/themeStore.js
+++ features/reduxStore/themeStore.js
@@ -1,7 +1,12 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 
-const val = { theme: "dark" }
+const val = {
+    theme: "dark",
+    primary_Color: "black",
+    text_Color: "white",
+    btn_Color: "black"
+}
 
 
 const themeSlice = createSlice({
@@ -10,6 +15,11 @@ const themeSlice = createSlice({
     reducers: {
         themeController: (state, action) => {
             state.value.theme === "dark" ? state.value.theme = "light" : state.value.theme = "dark";
+            state.value.theme === "dark" ? state.value.primary_Color = "black" : state.value.primary_Color = "white";
+            state.value.theme === "dark" ? state.value.text_Color = "white" : state.value.text_Color = "black";
+            state.value.theme === "dark" ? state.value.btn_Color = "black" : state.value.btn_Color = "white";
+
+
         }
     }
 })
